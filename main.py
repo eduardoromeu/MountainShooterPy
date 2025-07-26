145323f4b9945ab561d22891463010b2d8a70afa
Dudu <eduardoromeueberts@gmail.com>
2025-07-26 12:36:57 -0300

    Creating main menu

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('Setup Start')
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-
-print('Setup End')
-
-while True:
-  # Check for all events
-  for event in pygame.event.get():
-    if(event.type == pygame.QUIT):
-      pygame.quit() # close window
-      quit() # end pygame
-
-  pass
+game = Game()
+game.run()
